51cd467e4cb24c5f4e13cc325043292410875efd
Nitesh <niteshbharti005101@gmail.com>
2020-07-14 16:08:38 +0530

    changes

--- JS Practise/main.js
+++ JS Practise/main.js
@@ -22,6 +22,7 @@ var profile=(basic_info)=>{
 
 	var email=document.createElement("a");
 	email.href="mailto:"+basic_info.email;
+	email.classList.add("link");
 	email.textContent=basic_info.email;
 	child1.append(email);
 
@@ -30,6 +31,7 @@ var profile=(basic_info)=>{
 
 	var mobile=document.createElement("a");
 	mobile.href="tel:"+basic_info.mobile;
+	mobile.classList.add("link");
 	mobile.textContent=basic_info.mobile;
 	child1.append(mobile);
 
@@ -38,5 +40,36 @@ var profile=(basic_info)=>{
 	child1.append(address);
 }
 var details=(detail_info)=>{
-	//console.log(detail_info);	
+	//console.log(detail_info);
+	var summaryHeading=document.createElement("h2");
+	summaryHeading.textContent="Summary";
+	var hr1=document.createElement("hr");
+	child2.append(summaryHeading);
+	child2.append(hr1);
+
+	var list=document.createElement("ul");
+	detail_info.summary.map(i=>{
+		var listitem=document.createElement("li");
+		listitem.textContent=i;
+		list.append(listitem);
+	})
+	child2.append(list);
+
+	var skillHeading=document.createElement("h2");
+	skillHeading.textContent="Skills";
+	child2.append(skillHeading);
+	child2.append(hr1);
+
+	detail_info.Skills.map(j=>{
+		var skillType=document.createElement("h3");
+		skillType.textContent=j.type;
+		child2.append(skillType);
+
+		j.values.map(j_value=>{
+			var skillValue=document.createElement("span");
+			skillValue.textContent=j_value;
+			skillValue.classList.add("skill");
+			child2.append(skillValue);
+		})
+	})
 }
